ad4b143e526aa474aa2ef80d34db41fe0d5a6a96
Rosemary Rider <rosemary.rider@gmail.com>
2021-11-04 00:46:52 +0100

    add json-schema-to-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import {MyComponent} from "./MyComponent"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <MyComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
