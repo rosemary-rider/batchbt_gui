c3bc9bdbf58aa6ade38bdfd2cc69a353c3cec30c
Rosemary Rider <rosemary.rider@gmail.com>
2021-11-04 22:39:56 +0100

    ag-grid

--- src/Persons.tsx
+++ src/Persons.tsx
@@ -1,7 +1,8 @@
-import React, {useRef, useMemo} from 'react';
+import React, {useRef, useMemo, memo} from 'react';
 import {API_URL} from './api'
 import useFetch from 'use-http'
 import { Person } from "./types/person"
+import { ColDef } from 'ag-grid-community';
 import {AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
@@ -9,10 +10,11 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 export function Persons() {
   const gridRef = useRef<AgGridReact>(null);
   const { loading, error, data = [] } = useFetch<Person[]>(`${API_URL}/persons`, []);
-  const columnDefs = useMemo(
+  const columnDefs = useMemo<ColDef[]>(
     () => [
       { field: 'firstName'},
       { field: 'lastName'},
+      { field: 'age', cellRendererFramework: AgeCell, autoHeight: true }
     ],
     []
   );
@@ -28,6 +30,7 @@ export function Persons() {
     return <div>Error</div>;
   if (loading)
     return <div>Loading...</div>;
+  // use ag-grid reactui: https://www.ag-grid.com/react-data-grid/reactui/
   return (
     <AgGridReact
       // turn on AG Grid React UI
@@ -43,3 +46,12 @@ export function Persons() {
     />
   );
 }
+
+const AgeCell = memo<any>( (params: { value: number}) => {
+    return (
+          <div className="d-flex flex-column h-100">
+              {params.value} 
+              <button className="btn btn-primary my-2">hello</button>
+          </div>
+    );
+});
