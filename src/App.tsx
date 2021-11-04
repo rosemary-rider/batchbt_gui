ffa314522d7d804b5faaafa24eef69a7eacdfeae
Rosemary Rider <rosemary.rider@gmail.com>
2021-11-04 22:13:43 +0100

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import {Persons} from "./Persons"
-import './App.css';
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
-    <main className="flex-grow-1">
-      <Persons />
-    </main>
+  <React.StrictMode>
+    <BrowserRouter>
+<Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+</BrowserRouter>
+  </React.StrictMode>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+      <header>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+</header>
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <main className="flex-grow-1">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+function Home() {
+  return (
+    <div className="h-100">
+      <Persons/>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
   );
 }
 
